772837f157d703022a3aa3b7c8577bef549b76e7
Federico Navarrete <darklord.navarrete@gmail.com>
2020-08-10 08:22:05 +0200

    Upgrade
    - Upgraded to AndroidX.

--- sampleapp/src/main/java/angtrim/com/sampleapp/MainActivity.java
+++ sampleapp/src/main/java/angtrim/com/sampleapp/MainActivity.java
@@ -1,7 +1,7 @@
 package angtrim.com.sampleapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
